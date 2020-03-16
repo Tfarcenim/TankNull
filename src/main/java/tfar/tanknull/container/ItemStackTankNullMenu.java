18c7c1d6714387bc8233af31278296862bc905f8
Tfarcenim <44327798+Gimpler@users.noreply.github.com>
2020-03-16 12:13:36 -0400

    remove unneeded anon classes

--- src/main/java/tfar/tanknull/container/ItemStackTankNullMenu.java
+++ src/main/java/tfar/tanknull/container/ItemStackTankNullMenu.java
@@ -10,46 +10,36 @@ import tfar.tanknull.inventory.TankNullItemStackFluidStackHandler;
 
 public class ItemStackTankNullMenu extends ATankNullMenu {
 
-  public ItemStack te;
+	public ItemStack te;
 
-  public ItemStackTankNullMenu(int id, PlayerInventory inv) {
-    super(RegistryObjects.item_container.get(), id, inv);
-    te = inv.player.getHeldItem(Hand.MAIN_HAND);
-    addPlayerSlots(new InvWrapper(inv),inv.currentItem);
-  }
+	public ItemStackTankNullMenu(int id, PlayerInventory inv) {
+		super(RegistryObjects.item_container.get(), id, inv);
+		te = inv.player.getHeldItem(Hand.MAIN_HAND);
+		addPlayerSlots(new InvWrapper(inv), inv.currentItem);
+	}
 
-  protected void addPlayerSlots(InvWrapper playerinventory, int locked) {
-    int yStart = 32 + 18 * 3;
-    for (int row = 0; row < 3; ++row) {
-      for (int col = 0; col < 9; ++col) {
-        int x = 8 + col * 18;
-        int y = row * 18 + yStart;
-        this.addSlot(new SlotItemHandler(playerinventory, col + row * 9 + 9, x, y) {
-          @Override
-          public int getItemStackLimit(ItemStack stack) {
-            return Math.min(this.getSlotStackLimit(), stack.getMaxStackSize());
-          }
-        });
-      }
-    }
+	protected void addPlayerSlots(InvWrapper playerinventory, int locked) {
+		int yStart = 32 + 18 * 3;
+		for (int row = 0; row < 3; ++row) {
+			for (int col = 0; col < 9; ++col) {
+				int x = 8 + col * 18;
+				int y = row * 18 + yStart;
+				this.addSlot(new SlotItemHandler(playerinventory, col + row * 9 + 9, x, y));
+			}
+		}
 
-    for (int row = 0; row < 9; ++row) {
-      int x = 8 + row * 18;
-      int y = yStart + 58;
-      if (row != locked)
-        this.addSlot(new SlotItemHandler(playerinventory, row, x, y) {
-          @Override
-          public int getItemStackLimit(ItemStack stack) {
-            return Math.min(this.getSlotStackLimit(), stack.getMaxStackSize());
-          }
-        });
-      else
-        this.addSlot(new LockedSlot(playerinventory, row, x, y));
-    }
-  }
+		for (int row = 0; row < 9; ++row) {
+			int x = 8 + row * 18;
+			int y = yStart + 58;
+			if (row != locked)
+				this.addSlot(new SlotItemHandler(playerinventory, row, x, y));
+			else
+				this.addSlot(new LockedSlot(playerinventory, row, x, y));
+		}
+	}
 
-  @Override
-  public TankNullItemStackFluidStackHandler getHandler() {
-    return TankNullItemStackFluidStackHandler.create(te);
-  }
+	@Override
+	public TankNullItemStackFluidStackHandler getHandler() {
+		return TankNullItemStackFluidStackHandler.create(te);
+	}
 }
